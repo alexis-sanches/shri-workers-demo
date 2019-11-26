9602537e4cbf8bf06a5de4f2ce228fb1155ee027
alex-sannikov <alex-sannikov@yandex-team.ru>
2019-11-26 15:33:42 +0300

    fix

--- public/javascripts/sync/worker.js
+++ public/javascripts/sync/worker.js
@@ -3,7 +3,13 @@ let state = 1;
 
 onconnect = function(e) {
     const port = e.ports[0];
+
     clients.push(port);
+
+    port.postMessage({
+        connections: clients.length,
+        value: state
+    });
 
     port.addEventListener('message', function(e) {
         state = e.data;
